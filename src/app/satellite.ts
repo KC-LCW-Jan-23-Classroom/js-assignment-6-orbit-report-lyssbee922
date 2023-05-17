d9c428bd3782b6628528e5daa52bc6294fcf8f37
Elyssa Bezner <elyssabezner@MacBook-Pro-2.attlocal.net>
2023-05-17 12:26:27 -0500

    orbit counts

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -6,11 +6,7 @@ export class Satellite {
 	orbitType: string;
 	operational: boolean;
 
-	constructor(name: string, 
-		type: string, 
-		launchDate: string, 
-		orbitType: string, 
-		operational: boolean) {
+	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
 		this.type = type;
 		this.launchDate = launchDate;
